0e49360278c8fd0564046bbdd0c852f75fc78e6a
Swarnim Gupta <swarnimgupta2000@gmail.com>
2021-09-13 10:00:29 +0530

    resolving mixed content error

--- client/src/components/Movie/Movie.js
+++ client/src/components/Movie/Movie.js
@@ -13,11 +13,10 @@ const POSTER_PATH = 'https://image.tmdb.org/t/p/original';
 
 const MovieSection = ({searchmovies}) => {
   console.log('serachmoveis',searchmovies);
-   const { state, dispatch } = useContext(UserContext);
     const [ movies , setmovies] = useState(null);
     const [page, setpage] = useState(1);
     useEffect(() => {
-        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=f569e379d2c0bc46e541ef9379a90215&language=en-US&page=1`;
+        const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=f569e379d2c0bc46e541ef9379a90215&language=en-US&page=1';
         fetch(url).then((res) => res.json()).then((data) => {
             console.log(data);
             setmovies(data);
@@ -37,7 +36,7 @@ const MovieSection = ({searchmovies}) => {
         setTimeout(setmovies(searchmovies) , 2000);
         else
         {
-          const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=f569e379d2c0bc46e541ef9379a90215&language=en-US&page=1`;
+          const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=f569e379d2c0bc46e541ef9379a90215&language=en-US&page=1';
         fetch(url).then((res) => res.json()).then((data) => {
             setmovies(data);
         })
