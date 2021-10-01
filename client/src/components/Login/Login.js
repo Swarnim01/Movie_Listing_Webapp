c2d7fdb672f8c12dd3619e90b79bd6289909acce
Gurpreet <lostlegend2701@gmail.com>
2021-10-02 05:01:18 +0530

    Pevented token from getting  revealed in console

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -28,7 +28,6 @@ const Login = ({ toggleSign }) => {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         if (data.error) toast.error(data.error);
         else {
           toggleSign(true);
