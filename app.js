bb33a89555c751537fa40c532edb9a24744d0410
Swarnim01 <swarnimgupta2000@gmail.com>
2021-10-02 18:09:58 +0530

    Altering server code placement for right fetching

--- app.js
+++ app.js
@@ -39,12 +39,6 @@ app.use(
     credentials: true,
   })
 );
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -59,6 +53,12 @@ app.use("/logout", LogoutRouter);
 app.use("/protected", ProtectedRouter);
 app.use(FavouriteRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
