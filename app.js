a83e840ac34b843a605f5f9a1114cce4f53b13d8
Swarnim01 <swarnimgupta2000@gmail.com>
2021-10-02 16:46:06 +0530

    some update

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var createError = require('http-errors');
-var cors = require('cors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
+var createError = require("http-errors");
+var cors = require("cors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var SignUpRouter = require('./routes/signup');
-var SignInRouter = require('./routes/signin');
-const Users = require('./models/user');
-const ProtectedRouter = require('./routes/protected');
-const LogoutRouter = require('./routes/logout');
-const FavouriteRouter = require('./routes/favourite');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var SignUpRouter = require("./routes/signup");
+var SignInRouter = require("./routes/signin");
+const Users = require("./models/user");
+const ProtectedRouter = require("./routes/protected");
+const LogoutRouter = require("./routes/logout");
+const FavouriteRouter = require("./routes/favourite");
 
 const connect = mongoose.connect(process.env.MONGOURI, {
   useNewUrlParser: true,
@@ -23,53 +23,56 @@ const connect = mongoose.connect(process.env.MONGOURI, {
 });
 connect
   .then(() => {
-    console.log('connected to database');
+    console.log("connected to database");
   })
   .catch((err) => {
-    console.log('ERROR in connecting to DATABASE', err);
+    console.log("ERROR in connecting to DATABASE", err);
   });
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 app.use(
   cors({
     origin: [/localhost/, /127\.0\.0\.1/, /0\.0\.0\.0/],
     credentials: true,
   })
 );
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/signup', SignUpRouter);
-app.use('/signin', SignInRouter);
-app.use('/logout', LogoutRouter);
-app.use('/protected', ProtectedRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/signup", SignUpRouter);
+app.use("/signin", SignInRouter);
+app.use("/logout", LogoutRouter);
+app.use("/protected", ProtectedRouter);
 app.use(FavouriteRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
