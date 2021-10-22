0666a638a8ced37e62cbaebeb7771deb2be647c4
unknown <img_2020037@iiitm.ac.in>
2021-10-22 10:24:11 -0700

    corrected server app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use("/protected", ProtectedRouter);
 
 app.use(FavouriteRouter);
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
